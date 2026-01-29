5fdfd24bf526b534356dffc6ebd5023ebbb6a982
MurilloTercariol <murillonorris@hotmail.com>
2026-01-28 21:41:52 -0300

    Views de Documentos feitas

--- site-manuais/Data/ApplicationDbContext.cs
+++ site-manuais/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using site_manuais.Models;
 
 namespace site_manuais.Data
